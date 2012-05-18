def2f1eae637ba5ae88b980cf423e25c594ffd49
TimGLuz <timgluz@gmail.com>
2012-05-18 09:19:08 +0300

    first commit

---
+++ src/incanter_ml/core.clj
@@ -0,0 +1,1 @@
+(ns incanter-ml.core)
---
+++ test/incanter_ml/test/core.clj
@@ -0,0 +1,6 @@
+(ns incanter-ml.test.core
+  (:use [incanter-ml.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
